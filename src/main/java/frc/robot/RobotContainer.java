7f3cc81f720b31f05f8662d72f46ea6ab2a9cab8
Alexander Murray <amurray@sssas.org>
2026-03-01 12:02:18 -0500

    testing YAGSL_1

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,68 +1,18 @@
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import static edu.wpi.first.units.Units.*; 
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.units.measure.LinearVelocity;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.PS4Controller.Button;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.Constants.AutoConstants;
-import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OI;
 import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import java.util.List;
-import frc.robot.FuelSim;
-import frc.robot.Constants.Dimensions; 
-import frc.robot.Constants.*;
-import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.TurretSubsystem;
-import frc.robot.subsystems.ExtendedTranslation3d;
-
-
-
 
 public class RobotContainer {
     private final DriveSubsystem m_drive = new DriveSubsystem();
-    
-    private final FuelSim m_fuelSim = FuelSim.getInstance(); //need to figure out how to implement
 
-    private final IntakeSubsystem m_intake = new IntakeSubsystem();
-    private final TurretSubsystem m_turret = new TurretSubsystem();
-    private int fuelStored;
-    
-    
+    private final CommandXboxController m_controller =
+        new CommandXboxController(OI.DRIVER_CONTROLLER_PORT);
 
-    private final CommandXboxController m_controller = 
-        new CommandXboxController(Constants.OI.DRIVER_CONTROLLER_PORT);
-
-   
-    private boolean fieldCentric = true;
-    //new RunCommand(
-        // () -> m_robotDrive.drive(
-        //     -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
-        //     -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
-        //     -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
-        //     true),
-        // m_robotDrive));
     public RobotContainer() {
         m_drive.setDefaultCommand(
             new RunCommand(
@@ -72,127 +22,30 @@ public class RobotContainer {
                 double rotation = -MathUtil.applyDeadband(m_controller.getRightX(), OI.DEADBAND);
 
                 if (forward == 0 && strafe == 0 && rotation == 0) {
-                    m_drive.stopModules();
+                    m_drive.stop();
                 } else {
-                    m_drive.drive(forward, strafe, rotation, fieldCentric);
+                    m_drive.drive(forward, strafe, rotation);
                 }
             },
             m_drive)
-            );
-        
+        );
 
         configureBindings();
     }
 
     private void configureBindings() {
-        m_controller.a().onTrue(new InstantCommand(() -> {
-            fieldCentric = !fieldCentric;
-            SmartDashboard.putBoolean("Field Centric Enabled", fieldCentric);
-        }));
-
-        // Reset Heading
-        m_controller.start().onTrue(new InstantCommand(m_drive::zeroHeading, m_drive));
+        // X lock front right wheel
         m_controller.start().onTrue(
             new RunCommand(m_drive::setX, m_drive)
-                .until(() -> 
-                    Math.abs(m_controller.getLeftY()) > 0.05 || 
-                    Math.abs(m_controller.getLeftX()) > 0.05 || 
+                .until(() ->
+                    Math.abs(m_controller.getLeftY()) > 0.05 ||
+                    Math.abs(m_controller.getLeftX()) > 0.05 ||
                     Math.abs(m_controller.getRightX()) > 0.05
                 )
         );
     }
 
-    public double modifyAxis(double value) {
-        // Deadband is still good to keep drift away
-        if (Math.abs(value) < OI.DEADBAND) return 0;
-        
-        // Remove the squaring line: return Math.copySign(value * value, value);
-        // Return raw linear value instead:
-        return value;
-    }
-
-    public double getDriveForward(){
-        return m_controller.getLeftY();
-    }
-
-    public double getDriveTurn(){
-        return m_controller.getRightX();
-    }
-
     public Command getAutonomousCommand() {
-
-        // Replace with the actual autonomous command
-
-        return null; // Return your autonomous command here
-
+        return null;
     }
-
-
-
-
-
-
-        private void configureFuelSim() {
-            FuelSim instance = FuelSim.getInstance();
-            instance.spawnStartingFuel();
-            instance.registerRobot(
-                Dimensions.FULL_WIDTH,
-                Dimensions.FULL_LENGTH,
-                Dimensions.BUMPER_HEIGHT,
-                m_drive::getPose,
-                m_drive::getFieldSpeeds);
-            instance.registerIntake(
-                -Dimensions.div(Dimensions.FULL_LENGTH, 2), // Divide FULL_LENGTH by 2
-                Dimensions.div(Dimensions.FULL_LENGTH, 2), // Divide FULL_LENGTH by 2
-                -Dimensions.div(Dimensions.FULL_WIDTH, 2) + Dimensions.Inches.of(7), // Add 7 inches to half the width
-                -Dimensions.div(Dimensions.FULL_WIDTH, 2), // Divide FULL_WIDTH by 2
-                () -> m_intake.isFrountDeployed() && m_turret.simAbleToIntake(),
-                m_turret::simIntake
-            );
-            instance.registerIntake(
-                -Dimensions.div(Dimensions.FULL_LENGTH, 2), // Divide FULL_LENGTH by 2
-                Dimensions.div(Dimensions.FULL_LENGTH, 2), // Divide FULL_LENGTH by 2
-                Dimensions.div(Dimensions.FULL_WIDTH, 2),  // Divide FULL_WIDTH by 2
-                Dimensions.div(Dimensions.FULL_WIDTH, 2) + Dimensions.Inches.of(7), // Add 7 inches to half the width
-                () -> m_intake.isFrountDeployed() && m_turret.simAbleToIntake(),
-                m_turret::simIntake
-            );
-            instance.start();
-            SmartDashboard.putData(Commands.runOnce(() -> {
-                        FuelSim.getInstance().clearFuel();
-                        FuelSim.getInstance().spawnStartingFuel();
-                    })
-                    .withName("Reset Fuel")
-                    .ignoringDisable(true));
-        }
-    
-        public ExtendedTranslation3d launchVel(LinearVelocity velocity, Angle angle) {
-            // Convert the velocity and angle into a 3D velocity vector
-            double speed = velocity.in(MetersPerSecond);; // Assuming LinearVelocity has a method to get the value in meters per second
-            double angleRadians = angle.in(Radians); // Convert angle to radians using the appropriate method
-
-            // Calculate the velocity components
-            double vx = speed * Math.cos(angleRadians); // Horizontal velocity (X-axis)
-            double vz = speed * Math.sin(angleRadians); // Vertical velocity (Z-axis)
-
-            // Return the velocity as a Translation3d (Y-axis is 0 since it's not used here)
-            return new ExtendedTranslation3d(vx, 0, vz);
-        }
-
-
-        public void launchFuel(LinearVelocity vel, Angle angle) {
-            if (fuelStored == 0) return;
-            fuelStored--;
-            Pose2d pose2d = m_drive.getPose();
-
-            Pose3d robot = new Pose3d(
-                pose2d.getX(),
-                pose2d.getY(),
-                0.5, // Assume the fuel is launched from a height of 0.5 meters
-                new Rotation3d(0, 0, pose2d.getRotation().getRadians())
-            );
-
-            ExtendedTranslation3d initialPosition = new ExtendedTranslation3d(robot.getTranslation());
-            FuelSim.getInstance().spawnFuel(initialPosition, launchVel(vel, angle));
-        }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,22 +1,9 @@
 package frc.robot.subsystems;
 
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.config.PIDConstants;
-import com.pathplanner.lib.config.RobotConfig;
-import com.pathplanner.lib.controllers.PPHolonomicDriveController;
-
-import com.studica.frc.AHRS;
-
-import edu.wpi.first.math.filter.SlewRateLimiter;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Swerve;
@@ -24,176 +11,42 @@ import frc.robot.SwerveModule;
 
 public class DriveSubsystem extends SubsystemBase {
 
-    private final SwerveModule m_frontLeft = new SwerveModule(
-        Swerve.FL_DRIVE_ID, Swerve.FL_ANGLE_ID, Swerve.FL_CANCODER_ID,
-        Swerve.FL_OFFSET, Swerve.FL_INVERTED);
-
+    // Only the front right module for testing
     private final SwerveModule m_frontRight = new SwerveModule(
         Swerve.FR_DRIVE_ID, Swerve.FR_ANGLE_ID, Swerve.FR_CANCODER_ID,
         Swerve.FR_OFFSET, Swerve.FR_INVERTED);
 
-    private final SwerveModule m_rearLeft = new SwerveModule(
-        Swerve.BL_DRIVE_ID, Swerve.BL_ANGLE_ID, Swerve.BL_CANCODER_ID,
-        Swerve.BL_OFFSET, Swerve.BL_INVERTED);
+    public DriveSubsystem() {}
 
-    private final SwerveModule m_rearRight = new SwerveModule(
-        Swerve.BR_DRIVE_ID, Swerve.BR_ANGLE_ID, Swerve.BR_CANCODER_ID,
-        Swerve.BR_OFFSET, Swerve.BR_INVERTED);
-
-    private final AHRS m_navx = new AHRS(AHRS.NavXComType.kMXP_SPI);
-    private final Field2d m_field = new Field2d();
-
-    // Slew rate limiters for smoother acceleration (units/sec rate of change)
-    private final SlewRateLimiter m_xLimiter = new SlewRateLimiter(3.0);
-    private final SlewRateLimiter m_yLimiter = new SlewRateLimiter(3.0);
-    private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3.0);
-
-    private final SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(
-        Swerve.KINEMATICS,
-        getHeading(),
-        getModulePositions()
-    );
-
-    public DriveSubsystem() {
-        new Thread(() -> {
-            try { Thread.sleep(1000); zeroHeading(); } catch (Exception e) {}
-        }).start();
-        SmartDashboard.putData("Field", m_field);
-
-        // PathPlanner AutoBuilder configuration
-        RobotConfig config = null;
-        try {
-            config = RobotConfig.fromGUISettings();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        AutoBuilder.configure(
-            this::getPose,
-            this::resetPose,
-            this::getRobotRelativeSpeeds,
-            (speeds, feedforwards) -> driveRobotRelative(speeds),
-            new PPHolonomicDriveController(
-                new PIDConstants(5.0, 0.0, 0.0), // Translation PID
-                new PIDConstants(5.0, 0.0, 0.0)  // Rotation PID
-            ),
-            config,
-            () -> {
-                var alliance = DriverStation.getAlliance();
-                if (alliance.isPresent()) {
-                    return alliance.get() == DriverStation.Alliance.Red;
-                }
-                return false;
-            },
-            this
+    /**
+     * Drive using joystick inputs (-1 to 1). Robot-centric only.
+     * Computes kinematics for all 4 modules but only commands front right.
+     */
+    public void drive(double xSpeed, double ySpeed, double rot) {
+        ChassisSpeeds speeds = new ChassisSpeeds(
+            xSpeed * Swerve.MAX_SPEED,
+            ySpeed * Swerve.MAX_SPEED,
+            rot * Swerve.MAX_ANGULAR_SPEED
         );
-    }
 
-    /**
-     * Drive the robot using joystick inputs (-1 to 1).
-     * Applies slew rate limiting and scales to max speed.
-     */
-    public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
-        // Apply slew rate limiting for smoother acceleration
-        double limitedX = m_xLimiter.calculate(xSpeed);
-        double limitedY = m_yLimiter.calculate(ySpeed);
-        double limitedRot = m_rotLimiter.calculate(rot);
-
-        // Scale joystick (-1 to 1) to actual speeds (m/s and rad/s)
-        double xSpeedDelivered = limitedX * Swerve.MAX_SPEED;
-        double ySpeedDelivered = limitedY * Swerve.MAX_SPEED;
-        double rotDelivered = limitedRot * Swerve.MAX_ANGULAR_SPEED;
-
-        ChassisSpeeds speeds = fieldRelative
-            ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered, getHeading())
-            : new ChassisSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered);
-
+        // Kinematics output order: FL[0], FR[1], BL[2], BR[3]
         SwerveModuleState[] states = Swerve.KINEMATICS.toSwerveModuleStates(speeds);
         SwerveDriveKinematics.desaturateWheelSpeeds(states, Swerve.MAX_SPEED);
 
-        m_frontLeft.setState(states[0]);
+        // Only command front right
         m_frontRight.setState(states[1]);
-        m_rearLeft.setState(states[2]);
-        m_rearRight.setState(states[3]);
+
+        // Debug output
+        SmartDashboard.putNumber("FR Speed", states[1].speedMetersPerSecond);
+        SmartDashboard.putNumber("FR Angle", states[1].angle.getDegrees());
+        SmartDashboard.putNumber("FR Actual Angle", m_frontRight.getAngle().getDegrees());
     }
 
-    /**
-     * Drive the robot given robot-relative ChassisSpeeds (in m/s and rad/s).
-     * Used by PathPlanner — does NOT apply slew rate limiting or joystick scaling.
-     */
-    public void driveRobotRelative(ChassisSpeeds speeds) {
-        SwerveModuleState[] states = Swerve.KINEMATICS.toSwerveModuleStates(speeds);
-        SwerveDriveKinematics.desaturateWheelSpeeds(states, Swerve.MAX_SPEED);
-        m_frontLeft.setState(states[0]);
-        m_frontRight.setState(states[1]);
-        m_rearLeft.setState(states[2]);
-        m_rearRight.setState(states[3]);
-    }
-
-    @Override
-    public void periodic() {
-        m_odometry.update(getHeading(), getModulePositions());
-        m_field.setRobotPose(m_odometry.getPoseMeters());
-        SmartDashboard.putNumber("Gyro Heading", getHeading().getDegrees());
-    }
-
-    public Rotation2d getHeading() {
-        // We use getPitch() because the gyro is mounted vertically
-        return Rotation2d.fromDegrees(Swerve.INVERT_GYRO ? m_navx.getPitch() : -m_navx.getPitch());
-    }
-
-    public SwerveModulePosition[] getModulePositions() {
-        return new SwerveModulePosition[] {
-            m_frontLeft.getPosition(), m_frontRight.getPosition(),
-            m_rearLeft.getPosition(), m_rearRight.getPosition()
-        };
-    }
-
-    public SwerveModuleState[] getModuleStates() {
-        return new SwerveModuleState[] {
-            m_frontLeft.getState(), m_frontRight.getState(),
-            m_rearLeft.getState(), m_rearRight.getState()
-        };
-    }
-
-    public void zeroHeading() { m_navx.reset(); }
-
-    public Pose2d getPose() { return m_odometry.getPoseMeters(); }
-
-    public void resetPose(Pose2d pose) {
-        m_odometry.resetPosition(getHeading(), getModulePositions(), pose);
-    }
-
-    public ChassisSpeeds getRobotRelativeSpeeds() {
-        return Swerve.KINEMATICS.toChassisSpeeds(getModuleStates());
-    }
-
-    public ChassisSpeeds getFieldSpeeds() {
-        return ChassisSpeeds.fromFieldRelativeSpeeds(getRobotRelativeSpeeds(), getHeading());
-    }
-
-    public double getTurnRate() {
-        return m_navx.getRate() * (Swerve.INVERT_GYRO ? -1.0 : 1.0);
-    }
-
-    public void stopModules() {
-        m_frontLeft.stop();
+    public void stop() {
         m_frontRight.stop();
-        m_rearLeft.stop();
-        m_rearRight.stop();
     }
 
     public void setX() {
-        m_frontLeft.setState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
         m_frontRight.setState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
-        m_rearLeft.setState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
-        m_rearRight.setState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
-    }
-
-    public void resetEncoders() {
-        m_frontLeft.resetToAbsolute();
-        m_frontRight.resetToAbsolute();
-        m_rearLeft.resetToAbsolute();
-        m_rearRight.resetToAbsolute();
     }
 }
