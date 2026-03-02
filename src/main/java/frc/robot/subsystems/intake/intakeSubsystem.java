c3a1257e0e5d33f9348255c0f8f56078a26347b4
LR1117 <liamri28@sssas.org>
2026-03-02 16:39:36 -0500

    programing subsystems

--- src/main/java/frc/robot/subsystems/intake/intakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/intakeSubsystem.java
@@ -1,6 +1,50 @@
 package frc.robot.subsystems.intake;
 
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 public class intakeSubsystem {
-        
+        //set up for the intake, motors and methods to controle the motors.
+    private SparkFlex updown_motor = new SparkFlex(20, MotorType.kBrushless);
+    private SparkFlex roller_motor = new SparkFlex(21, MotorType.kBrushless);
+    SparkClosedLoopController pid = updown_motor.getClosedLoopController();
+
+    //intake up down methods
+
+    public void intakeUp(double rotations){
+        pid.setReference(rotations, ControlType.kPosition); // Updated to use the motor's closed-loop controller
+    }
+
+    public void intakeDown(double rotations){
+        pid.setReference(rotations, ControlType.kPosition); // Updated to use the motor's closed-loop controller
+    }
+
+    public void intakeStop(){
+        updown_motor.stopMotor();
+    }
+
+
+
+    //roller methods
+
+
+    public void rollerIn(double speed){
+        roller_motor.set(speed); //need to test
+    }
+
+    public void rollerOut(double speed){
+        roller_motor.set(-speed); //need to test
+    }
+
+    public void rollerStop(){
+        roller_motor.stopMotor();
+    }
+
+
 }
   
--- src/main/java/frc/robot/subsystems/turret/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/turret/TurretSubsystem.java
@@ -1,80 +1,75 @@
-//package frc.robot.subsystems.turret;
-// package frc.robot.subsystems;
+package frc.robot.subsystems.turret;
 
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-// import frc.robot.LimelightHelpers;
-// import frc.robot.Constants;
-// import com.revrobotics.spark.SparkFlex;
-// import com.revrobotics.spark.SparkLowLevel.MotorType;
-// import com.ctre.phoenix6.hardware.CANcoder;
+import frc.robot.LimelightHelpers;
+import frc.robot.Constants;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.ctre.phoenix6.hardware.CANcoder;
 
-// public class TurretSubsystem extends SubsystemBase {
+public class TurretSubsystem extends SubsystemBase {
     
-//         //electronics
-//     private SparkFlex turret_motor; 
-//     turret_motor = new SparkFlex(21, MotorType.kBrushless);
-//     private CANcoder turret_encoder = new CANcoder(31);
+        //electronics
+    private SparkFlex turret_motor1 = new SparkFlex(25, MotorType.kBrushless);
+    private SparkFlex turret_motor2 = new SparkFlex(25, MotorType.kBrushless);
 
+    public enum TurretState {
+        IDLE, SPIN, FIND_TARGET, TRACK_TARGET
+    }
 
+    public TurretSubsystem(){
+        double Tx;
+        double Ty;
+        boolean Tv;
+    }
 
+    public void periodic(){
+        double Tx = LimelightHelpers.getTX("limelight-vision");
+         Ty = LimelightHelpers.getTY("limelight-vision");
+         Tv = LimelightHelpers.getTV("limelight-vision");
+    }
+    public void startMotor(double speed){
+        turret_motor.set(speed);
+    }
 
-//     public enum TurretState {
-//         IDLE, SPIN, FIND_TARGET, TRACK_TARGET
-//     }
+    public void stopMotor(){
+        turret_motor.stopMotor();
+    }
 
-//     public TurretSubsystem(){
-//         double Tx;
-//         double Ty;
-//         boolean Tv;
-//     }
+    public double getAngle(){
+        return turret_encoder.getAbsolutePosition().getValueAsDouble();
+    }
 
-//     public void periodic(){
-//         double Tx = LimelightHelpers.getTX("limelight-vision");
-//          Ty = LimelightHelpers.getTY("limelight-vision");
-//          Tv = LimelightHelpers.getTV("limelight-vision");
-//     }
-//     public void startMotor(double speed){
-//         turret_motor.set(speed);
-//     }
+    public double getDistance(){
+        double limelightMountAngleDegrees =0;
+        double angleToGoalDegrees = limelightMountAngleDegrees + Ty;
+        double angleToGoalRadians = Math.toRadians(angleToGoalDegrees);
 
-//     public void stopMotor(){
-//         turret_motor.stopMotor();
-//     }
-
-//     public double getAngle(){
-//         return turret_encoder.getAbsolutePosition().getValueAsDouble();
-//     }
-
-//     public double getDistance(){
-//         double limelightMountAngleDegrees =0;
-//         double angleToGoalDegrees = limelightMountAngleDegrees + Ty;
-//         double angleToGoalRadians = Math.toRadians(angleToGoalDegrees);
-
-//         return(36 - Ty) / Math.tan(angleToGoalRadians);
+        return(36 - Ty) / Math.tan(angleToGoalRadians);
 
     
 
 
-//     }
+    }
 
-//     public double getTargetAngle(){
-//         double distance = getDistance();
-//         double TxRAD = Math.toRadians(Tx);
+    public double getTargetAngle(){
+        double distance = getDistance();
+        double TxRAD = Math.toRadians(Tx);
 
-//         double targetX = Tx + distance ;
-//     }
+        double targetX = Tx + distance ;
+    }
 
-//     public static double calculateVelocity(double g, double d, double thetaDegrees, double h0, double h) {
+    public static double calculateVelocity(double g, double d, double thetaDegrees, double h0, double h) {
     
-//     // Convert degrees to radians
-//     double theta = Math.toRadians(thetaDegrees);
+    // Convert degrees to radians
+    double theta = Math.toRadians(thetaDegrees);
 
-//     double numerator = g * Math.pow(d, 2);
+    double numerator = g * Math.pow(d, 2);
 
-//     double denominator = 2 * Math.pow(Math.cos(theta), 2) 
-//                          * (d * Math.tan(theta) + h0 - h);
+    double denominator = 2 * Math.pow(Math.cos(theta), 2) 
+                         * (d * Math.tan(theta) + h0 - h);
 
-//     return Math.sqrt(numerator / denominator);
-// }
-// }
+    return Math.sqrt(numerator / denominator);
+}
+}
