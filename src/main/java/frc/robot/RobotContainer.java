5d558e9c8d6132602e5f9a0758842507475f1ee4
LR1117 <liamri28@sssas.org>
2026-03-07 19:47:27 -0500

    changed auto path

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -299,7 +299,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("New Auto");
+    return drivebase.getAutonomousCommand("Just shoot Middle Blue");
   }
 
   public void setMotorBrake(boolean brake)
