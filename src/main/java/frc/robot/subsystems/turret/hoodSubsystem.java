e0caee0b151de2656a475b5b6251da29da5b7f56
TheIronman2131 <151246228+TheIronman2131@users.noreply.github.com>
2026-03-21 08:32:20 -0400

    this is bs

--- src/main/java/frc/robot/subsystems/turret/hoodSubsystem.java
+++ src/main/java/frc/robot/subsystems/turret/hoodSubsystem.java
@@ -3,41 +3,52 @@ package frc.robot.subsystems.turret;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class hoodSubsystem extends SubsystemBase {
+public class HoodSubsystem extends SubsystemBase {
     
-    private final Servo m_servoLeft;
-    private final Servo m_servoRight;
+    private final Servo m_servo;
 
-    public hoodSubsystem() {
-        m_servoLeft = new Servo(0); 
-        m_servoLeft.setBoundsMicroseconds(2100, 1508, 1500, 1492, 900);
-        m_servoRight = new Servo(1); 
-        m_servoRight.setBoundsMicroseconds(2100, 1508, 1500, 1492, 900);
-    
+    public HoodSubsystem() {
+        m_servo = new Servo(0); 
+        m_servo.setBoundsMicroseconds(2100, 1508, 1500, 1492, 900);
     }
-  
+    
+    public void stopHood() {
+        m_servo.setSpeed(0);
+    }
 
-//lj;kjkl  ;lkjlj lj 
+    // public void setSpeed() {
+    //     m_servo.setSpeed(0.3);
+        
+    // }
 
-    /** Returns the current angle of the left servo as a proxy for hood angle. */
-    public double getHoodAngle() {
-        return m_servoLeft.getAngle();
+    // public void setReversedSpeed(){
+    //     m_servo.setSpeed(-0.3);
+    // }
+
+    // public void setSpeed(double speed) {
+    //     m_servo.setSpeed(speed);
+    // }
+
+
+    // public void setReversedSpeed(double speed) {
+    //     m_servo.setSpeed(-speed);
+    // }
+
+    public void getHoodAngle() {
+        // Implement logic to get the current angle of the hood
     }
 
     public void setHoodZero(){
-        m_servoLeft.setAngle(0);
-        m_servoRight.setAngle(180); // ← inverted
+        m_servo.setAngle(0);
     }
-    
+
     public void setHoodMax(){
-        m_servoLeft.setAngle(180);
-        m_servoRight.setAngle(0); // ← inverted
+        m_servo.setAngle(180);
     }
-    
+
     public void setHoodAngle(double angle) {
-        m_servoLeft.setAngle(angle);
-        m_servoRight.setAngle(180 - angle); // ← inverted
+        m_servo.setAngle(angle);
     }
 
 
-} 
+}
